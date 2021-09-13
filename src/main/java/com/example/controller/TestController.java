64158a42959a954bb5ba541d08d333e4af0b0e13
wangyipeng <wangyipeng@xiaomi.com>
2021-09-13 16:20:47 +0800

    修复bug

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -2,14 +2,14 @@ package com.example.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
     @GetMapping(value = "/test")
-    public String test() {
-        System.out.println("wangyipeng");
-        return "sdafasdfsadf";
+    public String test(@RequestParam(name = "test") String test) {
+        return test;
     }
 }
